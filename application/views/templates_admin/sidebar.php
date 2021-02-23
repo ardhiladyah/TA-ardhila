f85cdf7626aec6b3f0a780316f8f4d7275308deb
AlKahfiHisyam <kahfiqwerty@gmail.com>
2021-02-23 11:10:45 +0700

    ganti test foto profile

--- application/views/templates_admin/sidebar.php
+++ application/views/templates_admin/sidebar.php
@@ -136,7 +136,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Selamat Datang Admin</span>
-                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
+                                <img class="img-profile rounded-circle" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Borobudur_Temple.jpg/320px-Borobudur_Temple.jpg">
                             </a>
 
                         </li>
